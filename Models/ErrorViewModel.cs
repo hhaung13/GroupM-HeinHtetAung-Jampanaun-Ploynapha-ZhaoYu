8974499311e1ad3e71cbe9cccb7e061f8abe7d04
lorra <lorra@JAYHEIN>
2024-05-25 22:58:54 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Driftr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
